b43e4e3b4bd01e625a9c514f2c5116a91b52eef2
Grant Simon <grant.simon-2@selu.edu>
2022-10-12 04:27:29 -0500

    Added a NavBar

--- FA22.P05.Web/ClientApp/src/components/NavBar/NavBar.js
+++ FA22.P05.Web/ClientApp/src/components/NavBar/NavBar.js
@@ -1,42 +1,96 @@
 import * as React from "react";
-import { AppBar, Toolbar, IconButton, List, ListItem, ListItemText, Container } from "@material-ui/core";
-import { Home } from "@material-ui/icons";
+import { AppBar, Toolbar, List, ListItem, ListItemText, Container, useTheme } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import SearchIcon from '@mui/icons-material/Search';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
     navbarDisplayFlex: {
-        display: `flex`,
-        justifyContent: `space-between`
+        display: `inline-flex`,
+        marginLeft: '0%',
     },
     navDisplayFlex: {
-        display: `flex`,
-        justifyContent: `space-between`
+        display: `inline-flex`,
+        paddingLeft: 575,
     },
     linkText: {
         textDecoration: `none`,
         textTransform: `uppercase`,
-        color: `white`
-    }
-});
+        background: 'black',
+        color: 'white',
+        borderRadius: '0px',
+        '&:hover': {
+            background: 'green',
+            boxShadow: "0px 2px 10px #888888",
+        },
+    },
+}));
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.black, 0.25),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.black, 0.50),
+    },
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'inline-flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'black',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'black',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(2.5, 0, 0, 0),
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '30ch',
+            '&:focus': {
+                width: '30ch',
+            },
+        },
+    },
+}));
 
 const navLinks = [
-    { title: `about us`, path: `/about-us` },
-    { title: `product`, path: `/product` },
-    { title: `blog`, path: `/blog` },
-    { title: `contact`, path: `/contact` },
-    { title: `faq`, path: `/faq` }
+    { title: `contact`, path: `/` },
+    { title: `register`, path: `/pages/SignUpPage` },
+    { title: `login`, path: `/pages/LoginPage` },
 ];
 
 const Header = () => {
     const classes = useStyles();
+    const theme = useTheme();
 
     return (
-        <AppBar position="static">
-            <Toolbar>
+        <AppBar position="static" style={{ background: 'linear-gradient(to right, #f7ff00, #db36a4)' }}>
+            <Toolbar >
                 <Container maxWidth="md" className={classes.navbarDisplayFlex}>
-                    <IconButton edge="start" color="inherit" aria-label="home">
-                        <Home fontSize="large" />
-                    </IconButton>
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search"
+                            inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </Search>
                     <List
                         component="nav"
                         aria-labelledby="main navigation"
@@ -44,7 +98,7 @@ const Header = () => {
                     >
                         {navLinks.map(({ title, path }) => (
                             <a href={path} key={title} className={classes.linkText}>
-                                <ListItem button>
+                                <ListItem button >
                                     <ListItemText primary={title} />
                                 </ListItem>
                             </a>
