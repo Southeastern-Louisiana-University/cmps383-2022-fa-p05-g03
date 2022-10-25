52690b8a40e91d5e5076493f9f3ba0e72ece1b43
Grant Simon <grant.simon-2@selu.edu>
2022-10-25 11:41:13 -0500

    Fixed routes/fixed signup and login pages

--- FA22.P05.Web/ClientApp/src/App.js
+++ FA22.P05.Web/ClientApp/src/App.js
@@ -2,9 +2,14 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Link } from "react-router-dom";
 
 import NavBar from './components/NavBar/NavBar';
+import HomePage from './pages/HomePage/HomePage';
 import SignUpPage from './pages/SignUpPage/SignUpPage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import ListingPage from './pages/ListingPage/ListingPage';
+import UserPage from './pages/UserPage/UserPage';
 
 const sections = [
     { title: 'CONSOLES', url: '/' },
@@ -20,9 +25,23 @@ export default function App() {
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <Container maxWidth="lg">
-                <NavBar title="GAME X-CHANGE" sections={sections} />
-                <main>
-                </main>
+                <NavBar title="GAME X-CHANGE" sections={sections} >
+                    <HomePage>
+                        <Link to="/HomePage">HomePage</Link>
+                    </HomePage>
+                    <SignUpPage>
+                        <Link to="/SignUpPage">SignUpPage</Link>
+                    </SignUpPage>
+                    <LoginPage>
+                        <Link to="/LoginPage">LoginPage</Link>
+                    </LoginPage>
+                    <UserPage>
+                        <Link to="/UserPage">UserPage</Link>
+                    </UserPage>
+                    <ListingPage>
+                        <Link to="/ListingPage">ListingPage</Link>
+                    </ListingPage>
+                </NavBar>
             </Container>
         </ThemeProvider>
     );
--- FA22.P05.Web/ClientApp/src/components/NavBar/NavBar.js
+++ FA22.P05.Web/ClientApp/src/components/NavBar/NavBar.js
@@ -6,60 +6,70 @@ import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import styles from "./NavBar.css"
 import TextField from '@mui/material/TextField';
+import { useNavigate } from "react-router-dom";
 
 
-function Header(props) {
-    const { sections, title } = props;
+    export default function Header(props) {
+        const { sections, title } = props;
 
-    return (
-        <React.Fragment>
-            <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                <Button variant="fill" size="small">LOGIN</Button>
-                <Typography
-                    component="h2"
-                    variant="h5"
-                    color="inherit"
-                    align="center"
-                    noWrap
-                    sx={{ flex: 1 }}
-                >
-                    {title}
-                </Typography>
-                <Button variant="fill" size="small" >
-                    SIGNUP
-                </Button>
-            </Toolbar>
-            <Toolbar
-                component="nav"
-                variant="dense"
-                sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
-            >
-                <TextField placeholder="SEARCH" variant="outlined" color="secondary" size="small" focused />
-                {sections.map((section) => (
-                    <Link
+        let navigate = useNavigate();
+        const SignUpPage = () => {
+            let path = `./SignUpPage`;
+            navigate(path);
+        }
+
+        const LoginPage = () => {
+            let path = `./LoginPage`;
+            navigate(path);
+        }
+
+        return (
+            <React.Fragment>
+                <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                    <Button variant="fill" size="small" onClick={LoginPage}>LOGIN</Button>
+                    <Typography
+                        component="h2"
+                        variant="h5"
                         color="inherit"
+                        align="center"
                         noWrap
-                        key={section.title}
-                        variant="body2"
-                        href={section.url}
-                        sx={{ p: 1, flexShrink: 0 }}
+                        sx={{ flex: 1 }}
                     >
-                        {section.title}
-                    </Link>
-                ))}
-            </Toolbar>
-        </React.Fragment>
-    );
-}
+                        {title}
+                    </Typography>
+                    <Button variant="fill" size="small" onClick={SignUpPage} >
+                        SIGNUP
+                    </Button>
+                </Toolbar>
+                <Toolbar
+                    component="nav"
+                    variant="dense"
+                    sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
+                >
+                    <TextField placeholder="SEARCH" variant="outlined" color="secondary" size="small" focused />
+                    {sections.map((section) => (
+                        <Link
+                            color="inherit"
+                            noWrap
+                            key={section.title}
+                            variant="body2"
+                            href={section.url}
+                            sx={{ p: 1, flexShrink: 0 }}
+                        >
+                            {section.title}
+                        </Link>
+                    ))}
+                </Toolbar>
+            </React.Fragment>
+        );
+    }
 
-Header.propTypes = {
-    sections: PropTypes.arrayOf(
-        PropTypes.shape({
-            title: PropTypes.string.isRequired,
-            url: PropTypes.string.isRequired,
-        }),
-    ).isRequired,
-    title: PropTypes.string.isRequired,
-};
-
-export default Header;
+    Header.propTypes = {
+        sections: PropTypes.arrayOf(
+            PropTypes.shape({
+                title: PropTypes.string.isRequired,
+                url: PropTypes.string.isRequired,
+            }),
+        ).isRequired,
+        title: PropTypes.string.isRequired,
+    };
