a4f5ae9f92717cae350641a66a4cb8f8b32a5aaf
Grant Simon <grant.simon-2@selu.edu>
2022-10-26 02:37:25 -0500

    Added logo and page styling

--- FA22.P05.Web/ClientApp/src/pages/LoginPage/LoginPage.js
+++ FA22.P05.Web/ClientApp/src/pages/LoginPage/LoginPage.js
@@ -1,15 +1,16 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import logo from '../../components/logo.png';
+import background from '../../components/background.mp4';
 
 const theme = createTheme();
 
@@ -24,67 +25,85 @@ export default function Login() {
     };
 
     return (
-        <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Login
-                    </Typography>
-                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    autoComplete="email"
-                                />
+        <div>
+            <video src={background} autoPlay loop muted style={{
+                position: "absolute",
+                width: "100%",
+                left: "50%",
+                top: "50%",
+                height: "100%",
+                objectFit: "cover",
+                transform: "translate(-50%, -50%)",
+                zIndex: "-1"
+            }} />
+            <ThemeProvider theme={theme}>
+                <Container component="main" maxWidth="xs" >
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            background: 'black',
+                            padding: '25px 25px 25px 25px'
+                        }}
+                    >
+                        <a href="/">
+                            <img src={logo} alt="Game X-Change" width="250" height="100" />
+                        </a>
+                        <Typography component="h1" variant="h5" color="white">
+                            LOGIN
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} >
+                                    <TextField sx={{ input: { background: "white", color: "black" } }}
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                        variant="filled"
+                                        color="secondary"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField sx={{ input: { background: "white", color: "black" } }}
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="new-password"
+                                        variant="filled"
+                                        color="secondary"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}> </Grid>
                             </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="new-password"
-                                />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                                color="inherit"
+                            >
+                                Submit
+                            </Button>
+                            <Grid container justifyContent="flex-end">
+                                <Grid item>
+                                    <Link href="./SignUpPage" variant="body2" >
+                                        Don't have an account? Sign up
+                                    </Link>
+                                </Grid>
                             </Grid>
-                            <Grid item xs={12}> </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            color="inherit"
-                        >
-                            Submit
-                        </Button>
-                        <Grid container justifyContent="flex-end">
-                            <Grid item>
-                                <Link href="./SignUpPage" variant="body2">
-                                    Don't have an account? Sign up
-                                </Link>
-                            </Grid>
-                        </Grid>
+                        </Box>
                     </Box>
-                </Box>
-            </Container>
-        </ThemeProvider>
+                </Container>
+            </ThemeProvider>
+        </div>
     );
 }
