265289cd215025644eaa84703e5e6700621a037d
Brennon O'Neill <brennon.oneill@selu.edu>
2022-11-04 00:16:06 -0500

    kinda getting there.  some bugs that need to be squashed.

---
+++ FA22.P05.Web/ClientApp/src/constants/types.js
@@ -0,0 +1,27 @@
+﻿
+export type ApiResponse<T> = {
+    data: T;
+    errors: Error[];
+    hasErrors: boolean;
+};
+
+export type Error = {
+    property: string;
+    message: string;
+};
+
+export type AnyObject = {
+    [index: string]: any;
+};
+
+export  ListingDTO  = {
+    firstName: string;
+    lastName: string;
+    socialSecurityNumber: string;
+    dateOfBirth: string;
+    dateOfEmployment: string;
+    licenseNumber: string;
+    licenseExpiration: string;
+    medicalExpiration: string;
+    truckId?: number;
+};
--- FA22.P05.Web/ClientApp/src/pages/ListingPage/ListingPage.js
+++ FA22.P05.Web/ClientApp/src/pages/ListingPage/ListingPage.js
@@ -8,13 +8,52 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import axios from "axios";
+import { ApiResponse, ListingDto } from "../../constants/types";
+import { useEffect, useState } from "react";
 
 import logo from "../../components/logo.png";
 
 const theme = createTheme();
 
-export default function Login() {
-    
+export const Listings = () => {
+    const [listings, setListings] = useState < ApiResponse() < ListingDto();
+    const [didLoad, setDidLoad] = useState(false);
+    const [Error, setError] = useState("");
+   // const history = useHistory();
+
+    useEffect(() => {
+        axios({
+            method: 'get',
+            url: `/api/listings`,
+            responseType: 'stream'
+        })
+                .then(function (response) {
+                    console.log("message");
+                    if (response.data.hasErrors) {
+                        response.data.errors.forEach((err) => {
+                            console.error(`${err.property}: ${err.message}`);
+                            setError(err.message);
+                        });
+                    }
+                    setListings(response.data);
+                    setDidLoad(true);
+                });
+    }, []);
+
+    if (Error !== "") {
+        return <div>Error Occurred</div>;
+    } else if (!didLoad) {
+        return (
+            <div style={{ height: "100%", width: "100%" }} className="ui segment">
+                <div className="ui active dimmer">
+                    <div className="ui text loader">Loading</div>
+                </div>
+            </div>
+        );
+    }
+    const listingsToShow = listings?.data;
+
     return (
         <div>
             <ThemeProvider theme={theme}>
@@ -41,6 +80,9 @@ export default function Login() {
                         noValidate
                         sx={{ mt: 3 }}
                         >
+                            <Container>
+                                
+                            </Container>
                             <Typography component="h1" variant="h5" color="white">
                                 Listing 1
                             </Typography>
