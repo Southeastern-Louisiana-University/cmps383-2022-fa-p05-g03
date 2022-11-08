56ad43a4238cc6b006dbde55cbc8345d9fecd3ed
Brennon O'Neill <brennon.oneill@selu.edu>
2022-11-08 11:49:23 -0600

    css

--- FA22.P05.Web/ClientApp/src/pages/ListingPage/ListingPage.js
+++ FA22.P05.Web/ClientApp/src/pages/ListingPage/ListingPage.js
@@ -1,48 +1,34 @@
-﻿import * as React from "react";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import axios from "axios";
-import { ApiResponse, ListingDto } from "../../constants/types";
-import { useEffect, useState } from "react";
+﻿import "animate.css"
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import { useHistory } from "react-router-dom"
+import { Button, Segment } from "semantic-ui-react"
+import { baseUrl } from "../../../constants/baseUrl"
+import { routes } from "../../../routes/config"
+import "../ListingPage/ListingPage.css"
 
-import logo from "../../components/logo.png";
-
-const theme = createTheme();
-
-export const Listings = () => {
-    const [listings, setListings] = useState < ApiResponse() < ListingDto();
-    const [didLoad, setDidLoad] = useState(false);
-    const [Error, setError] = useState("");
-   // const history = useHistory();
+export const Listing = () => {
+    const [listings, setlistings] = useState()
+    const [didLoad, setDidLoad] = useState(false)
+    const [Error, setError] = useState("")
+    const history = useHistory()
 
     useEffect(() => {
-        axios({
-            method: 'get',
-            url: `/api/listings`,
-            responseType: 'stream'
+        axios.get(`${baseUrl}/api/listings`).then(response => {
+            console.log("message")
+            if (response.data.hasErrors) {
+                response.data.errors.forEach(err => {
+                    console.error(`${err.property}: ${err.message}`)
+                    setError(err.message)
+                })
+            }
+            setlistings(response.data)
+            setDidLoad(true)
         })
-                .then(function (response) {
-                    console.log("message");
-                    if (response.data.hasErrors) {
-                        response.data.errors.forEach((err) => {
-                            console.error(`${err.property}: ${err.message}`);
-                            setError(err.message);
-                        });
-                    }
-                    setListings(response.data);
-                    setDidLoad(true);
-                });
-    }, []);
+    }, [])
 
     if (Error !== "") {
-        return <div>Error Occurred</div>;
+        return <div>Error Occurred</div>
     } else if (!didLoad) {
         return (
             <div style={{ height: "100%", width: "100%" }} className="ui segment">
@@ -50,54 +36,35 @@ export const Listings = () => {
                     <div className="ui text loader">Loading</div>
                 </div>
             </div>
-        );
+        )
     }
-    const listingsToShow = listings?.data;
+    const listingsToShow = listings?.data
 
     return (
-        <div>
-            <ThemeProvider theme={theme}>
-                <Container component="main" maxWidth="xs">
-                    <CssBaseline />
-                    <Box
-                        sx={{
-                            marginTop: 6,
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center",
-                            background: "black",
-                            padding: "25px 25px 25px 25px",
-                            border: 3,
-                            borderColor: "white",
-                        }}
-                    >
-                      
-                        <Typography component="h1" variant="h3" color="white">
-                            All Listings
-                        </Typography>
-                    <Box
-                        component="list"
-                        noValidate
-                        sx={{ mt: 3 }}
-                        >
-                            <Container>
-                                
-                            </Container>
-                            <Typography component="h1" variant="h5" color="white">
-                                Listing 1
-                            </Typography>
-                            <Typography component="h1" variant="h5" color="white">
-                                Listing 2
-                            </Typography>
-                            <Typography component="h1" variant="h5" color="white">
-                                Listing 3
-                            </Typography>
-
-                        
-                    </Box>
-                </Box>
-            </Container>
-        </ThemeProvider>
-        </div >
-    );
+        <div className=".flex-box-centered-content-listing-getById">
+            {employeesToShow &&
+                employeesToShow.map(x => {
+                    return (
+                        <div className="flex-row-fill-listing-getById">
+                            <Segment raised className="employee-getById-segments">
+                                <div>{`First Name: ${x.firstName}`}</div>
+                                <div>{`Last Name: ${x.lastName}`}</div>
+                                <div className="button-container-listing-getById">
+                                    <Button
+                                        color="violet"
+                                        onClick={() => {
+                                            history.push(
+                                                routes.listingGetById.replace(":id", `${x.id}`)
+                                            )
+                                        }}
+                                    >
+                                        See More
+                                    </Button>
+                                </div>
+                            </Segment>
+                        </div>
+                    )
+                })}
+        </div>
+    )
 }
