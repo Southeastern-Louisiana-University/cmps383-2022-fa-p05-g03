85ecc123f61fa9a234d1d444e2d6b75de8fc79bf
Grant Simon <grant.simon-2@selu.edu>
2022-11-09 11:23:55 -0600

    Formatting

--- FA22.P05.Web/ClientApp/src/pages/ListingPage/ListingPage.js
+++ FA22.P05.Web/ClientApp/src/pages/ListingPage/ListingPage.js
@@ -7,64 +7,64 @@ import { render } from "../../index";
 import "./ListingPage.css";
 
 export const Listing = () => {
-    const navigate = useNavigate();
-    const [listings, setlistings] = useState()
-    const [didLoad, setDidLoad] = useState(false)
-    const [Error, setError] = useState("")
+  const navigate = useNavigate();
+  const [listings, setlistings] = useState();
+  const [didLoad, setDidLoad] = useState(false);
+  const [Error, setError] = useState("");
 
-    useEffect(() => {
-        axios.get(`${baseUrl}/api/listings`).then(response => {
-            console.log("message")
-            if (response.data.hasErrors) {
-                response.data.errors.forEach(err => {
-                    console.error(`${err.property}: ${err.message}`)
-                    setError(err.message)
-                })
-            }
-            setlistings(response.data)
-            setDidLoad(true)
-        })
-    }, [])
+  useEffect(() => {
+    axios.get(`${baseUrl}/api/listings`).then((response) => {
+      console.log("message");
+      if (response.data.hasErrors) {
+        response.data.errors.forEach((err) => {
+          console.error(`${err.property}: ${err.message}`);
+          setError(err.message);
+        });
+      }
+      setlistings(response.data);
+      setDidLoad(true);
+    });
+  }, []);
 
-    if (Error !== "") {
-        return <div>Error Occurred</div>
-    } else if (!didLoad) {
-        return (
-            <div style={{ height: "100%", width: "100%" }} className="ui segment">
-                <div className="ui active dimmer">
-                    <div className="ui text loader">Loading</div>
-                </div>
-            </div>
-        )
-    }
-    const listingsToShow = listings?.data
-
+  if (Error !== "") {
+    return <div>Error Occurred</div>;
+  } else if (!didLoad) {
     return (
-        <div className=".flex-box-centered-content-listing-getById">
-            {listingsToShow &&
-                listingsToShow.map(x => {
-                    return (
-                        <div className="flex-row-fill-listing-getById">
-                            <Segment raised className="listing-getById-segments">
-                                <div></div>
-                                <div></div>
-                                <div className="button-container-listing-getById">
-                                    <Button
-                                        color="violet"
-                                        onClick={() => {
-                                            navigate.push(
-                                                render.listingGetById.replace(":id", `${x.id}`)
-                                            )
-                                        }}
-                                    >
-                                        See More
-                                    </Button>
-                                </div>
-                            </Segment>
-                        </div>
-                    )
-                })}
+      <div style={{ height: "100%", width: "100%" }} className="ui segment">
+        <div className="ui active dimmer">
+          <div className="ui text loader">Loading</div>
         </div>
-    )
-}
+      </div>
+    );
+  }
+  const listingsToShow = listings?.data;
+
+  return (
+    <div className=".flex-box-centered-content-listing-getById">
+      {listingsToShow &&
+        listingsToShow.map((x) => {
+          return (
+            <div className="flex-row-fill-listing-getById">
+              <Segment raised className="listing-getById-segments">
+                <div></div>
+                <div></div>
+                <div className="button-container-listing-getById">
+                  <Button
+                    color="violet"
+                    onClick={() => {
+                      navigate.push(
+                        render.listingGetById.replace(":id", `${x.id}`)
+                      );
+                    }}
+                  >
+                    See More
+                  </Button>
+                </div>
+              </Segment>
+            </div>
+          );
+        })}
+    </div>
+  );
+};
 export default Listing;
