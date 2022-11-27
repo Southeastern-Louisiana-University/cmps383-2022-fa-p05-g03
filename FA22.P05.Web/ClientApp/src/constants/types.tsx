ae7aff9f302d485ab03ca2883ca48e648c46d5ae
Brennon O'Neill <brennon.oneill@selu.edu>
2022-11-27 15:48:38 -0600

    listing page works. need to show listigs now.

---
+++ FA22.P05.Web/ClientApp/src/constants/types.tsx
@@ -0,0 +1,29 @@
+﻿export type Error = {
+    property: string;
+    message: string;
+};
+export type ApiResponse<T> = {
+    data: T;
+    errors: Error[];
+    hasErrors: boolean;
+}
+export type ListingDto = {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    startUtc: Date;
+    endUtc: Date;
+};
+
+export type LoginDto = {
+    username: string;
+    password: string;
+};
+
+export type BidDto = {
+    id: number;
+    userId: number;
+    bidAmount: number;
+    listingId: number;
+}
--- FA22.P05.Web/ClientApp/src/pages/ListingPage/ListingPage.tsx
+++ FA22.P05.Web/ClientApp/src/pages/ListingPage/ListingPage.tsx
@@ -1,14 +1,100 @@
+import { TextField } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { HeaderButton } from "../../components/MUI/HeaderButton";
+import { NavButton } from "../../components/MUI/NavButton";
+import { PageRoutes } from "../../models/PageRoutes";
+import logo from "../../resources/logo.png";
+import styles from "./_ListingPage.module.scss";
+import { useEffect, useState } from "react";
+import { ListingDto } from "../../constants/types";
 
-// import styles from './_ListingPage.module.scss';
-
-/**
- * <description here>
- */
+//Home Page for the App
 export function ListingPage(): React.ReactElement {
-  return (
-    <div>
-      <h1>ListingPage</h1>
-    </div>
-  );
-}
+    const [listings, setListings] = useState<ListingDto[]>([]);
+
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axios.get<ListingDto[]>("/api/listings/active").then((response) => {
+            setListings(response.data);
+        });
+    }, []);
+
+    const [listing, setListing] = useState<ListingDto>();
+
+
+    //Navigate to Login Page
+    const navigateToLoginPage = (): void => {
+        navigate(PageRoutes.LOGIN_PAGE);
+    };
+
+    //Navigate to Signup Page
+    const navigateToSignupPage = (): void => {
+        navigate(PageRoutes.SIGNUP_PAGE);
+    };
+
+    //Navigate to Listing Page
+    const navigateToListingPage = (): void => {
+        navigate(PageRoutes.LISTING_PAGE);
+    };
+
+    //Navigate to User Page
+    const navigateToUserPage = (): void => {
+        navigate(PageRoutes.USER_PAGE);
+    };
+
+    //Navigate to Checkout Page
+    const navigateToCheckoutPage = (): void => {
+        navigate(PageRoutes.CHECKOUT_PAGE);
+    };
+        
+
+        const listingsToShow = listings;
+
+        return (
+            <div className={styles.listingPage}>
+                {/* Custom Toolbar */}
+                <div className={styles.toolbar}>
+                    {/* Login, Signup, & Logo */}
+                    <div className={styles.topHalf}>
+                        <HeaderButton
+                            onClick={navigateToLoginPage}
+                            size="small"
+                        >
+                            LOGIN
+                        </HeaderButton>
+                        <img
+                            alt="Game X-Change"
+                            height="100"
+                            src={logo}
+                            width="250"
+                        />
+                        <HeaderButton
+                            onClick={navigateToSignupPage}
+                            size="small"
+                        >
+                            SIGNUP
+                        </HeaderButton>
+                    </div>
+
+                    {/* Search Bar & Links */}
+                    <div className={styles.bottomHalf}>
+                        <TextField
+                            color="primary"
+                            focused={true}
+                            placeholder="SEARCH"
+                            size="small"
+                            sx={{ input: { background: "black", color: "white" }, width: "25%" }}
+                            variant="outlined"
+                        />
+                        <NavButton onClick={navigateToListingPage}> ALL LISTINGS</NavButton>
+                        <NavButton onClick={navigateToUserPage}>MY LISTINGS</NavButton>
+                        <NavButton onClick={navigateToCheckoutPage}>CHECKOUT</NavButton>
+                    </div>
+                </div>
+            </div>
+        );
+    }
