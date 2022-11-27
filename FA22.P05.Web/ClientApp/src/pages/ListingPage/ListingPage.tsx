9c703f6e245506712f64a7bdac8c4de180de3318
Grant Simon <grant.simon-2@selu.edu>
2022-11-27 16:26:50 -0600

    Minor Edits

--- FA22.P05.Web/ClientApp/src/pages/ListingPage/ListingPage.tsx
+++ FA22.P05.Web/ClientApp/src/pages/ListingPage/ListingPage.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 
-// import styles from './_ListingPage.module.scss';
-
-/**
- * <description here>
- */
 export function ListingPage(): React.ReactElement {
   return (
     <div>
-      <h1>ListingPage</h1>
+      <h1>Listing Page</h1>
     </div>
   );
 }
