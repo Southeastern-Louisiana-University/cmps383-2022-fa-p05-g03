f7b04326991a6111d06c15619522a99cc0f5cf0f
oretha francis <francis.oretha@selu.edu>
2022-11-27 21:08:18 -0600

    paging

--- FA22.P05.Web/ClientApp/src/pages/ListingPage/ListingPage.tsx
+++ FA22.P05.Web/ClientApp/src/pages/ListingPage/ListingPage.tsx
@@ -1,14 +1,32 @@
 import React from "react";
 
-// import styles from './_ListingPage.module.scss';
+import styles from './_ListingPage.module.scss';
 
 /**
  * <description here>
  */
 export function ListingPage(): React.ReactElement {
   return (
-    <div>
-      <h1>ListingPage</h1>
-    </div>
+      <div className={styles['product-details']}>
+          ProductDetails
+           <div className={styles['gallary']}>
+        <div className={styles['main-image']}>
+
+        </div>
+        <div className={styles['image-options']}>
+            <div className={styles['image']} />
+            <div className={styles['image']} />
+            <div className={styles['image']} />
+            <div className={styles['image']} />
+            <div className={styles['image']} />
+        </div>
+          </div>
+
+          <nav className={styles['navbar']}>
+              <span>Logo</span>
+              <span>My Cart</span>
+          </nav>
+      </div>
+
   );
 }
